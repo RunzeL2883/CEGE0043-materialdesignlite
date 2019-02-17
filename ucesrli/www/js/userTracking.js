5a006d9f05fd8b94d12882a643b669f5465d6c25
Runze Liu <runze.liu.18@ucl.ac.uk>
2019-02-17 15:44:54 -0500

    adwithin 100m alert

--- ucesrli/www/js/userTracking.js
+++ ucesrli/www/js/userTracking.js
@@ -26,7 +26,7 @@ function getDistanceFromPoint(position) {
     var lng = -0.13818;
     // return the distance in kilometers
     var distance = calculateDistance(position.coords.latitude, position.coords.longitude, lat,lng, 'K');
-    document.getElementById('showDistance').innerHTML = "Distance: " + distance;
+    if (distance <= 0.1) {alert("You are with 100m of UCL.")}
 }
 // code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
 function calculateDistance(lat1, lon1, lat2, lon2, unit) {
