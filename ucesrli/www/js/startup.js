961f3417f1927ac2182db927e86162cdd8d22616
Runze Liu <runze.liu.18@ucl.ac.uk>
2019-02-17 14:56:00 -0500

    all ththe code needed

--- ucesrli/www/js/startup.js
+++ ucesrli/www/js/startup.js
@@ -1,3 +1,16 @@
-function trackAndCircle () {
-	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>Here's your position!</b>").openPopup();
+unction trackLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(showPosition);
+	}
+	else {
+		document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+var userMarker;
+function showPosition(position) {
+	if (userMarker){
+		mymap.removeLayer(userMarker);
+	}
+	userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap).bindPopup("<b>You were here</b>");
+	document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 }
