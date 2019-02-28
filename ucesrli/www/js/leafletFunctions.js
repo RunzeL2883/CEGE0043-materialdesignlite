78ce4f1bcb1b093370fa1bdf232d8da638e3315d
Runze Liu <runze.liu.18@ucl.ac.uk>
2019-02-27 19:39:32 -0500

    get hold to the Earthquakes data

--- ucesrli/www/js/leafletFunctions.js
+++ ucesrli/www/js/leafletFunctions.js
@@ -21,3 +21,58 @@ function addPointLinePoly() {
 			fillOpacity: 0.5
 		}).addTo(mymap).bindPopup("I am a polygon.");
 }
+
+var client;
+var earthquakes;
+
+// create the code to get the Earthquakes data using an XMLHttpRequest
+function getEarthquakes() {
+	client = new XMLHttpRequest();
+	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+	client.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work 
+	client.send();
+}
+
+function loadEarthquakeData() {
+    // call the getEarthquakes code
+    // keep the alert message so that we know something is happening
+    alert("Loading Earthquakes");
+    getEarthquakes();
+}
+
+function earthquakeResponse() {
+    // this function listens out for the server to say that the data is ready - i.e. has state 4
+    if (client.readyState == 4) {
+        // once the data is ready, process the data
+        var earthquakedata = client.responseText;
+        loadEarthquakelayer(earthquakedata);
+    }
+}
+
+var earthquakelayer;
+
+function loadEarthquakelayer(earthquakedata) {
+    // convert the text received from the server to JSON
+    var earthquakejson = JSON.parse(earthquakedata );
+    earthquakes = earthquakejson;
+
+    // load the geoJSON layer
+    earthquakelayer = L.geoJson(earthquakejson,
+    {
+        // use point to layer to create the points
+        pointToLayer: function (feature, latlng)
+        {
+            // look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+             // also include a pop-up that shows the place value of the earthquakes
+            if (feature.properties.mag > 1.75) {
+            	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place+"</b>");
+            }
+
+            else {
+                // magnitude is 1.75 or less
+                return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place+"</b>");;
+            }
+        },
+    }).addTo(mymap);
+    mymap.fitBounds(earthquakelayer.getBounds());
+}
--- ucesrli/www/js/startup.js
+++ ucesrli/www/js/startup.js
@@ -1,6 +1,7 @@
 function trackAndCircle() {
 	addPointLinePoly();
 	trackLocation();
+	getEarthquakes()
 }
 function startup() {
 	document.addEventListener('DOMContentLoaded', function() {
